b4d58b668b16ad3a95af5184eaee558cf5c02081
KowsalyaKumaresh <kowsalyakumar01012001@gmail.com>
2022-06-08 09:21:32 +0530

    add clone

---
+++ src/com/chainsys/bestPractices/clone/Student.java
@@ -0,0 +1,16 @@
+package com.chainsys.bestPractices.clone;
+
+public class Student// implements Cloneable  
+{
+	public String name ;
+	public int age;
+	
+	 @Override 
+	 
+	 protected Object clone() throws CloneNotSupportedException{
+	 
+		 
+		 return super.clone();
+	 }
+
+}
